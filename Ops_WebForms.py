4bcd3da113fa1c33b5a0544292148bf19073b6ae
Tomas <tomasmaggio@gmail.com>
2016-11-22 15:14:58 +1300

    Create Ops_WebForms.py

---
+++ Ops_WebForms.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+app = Flask(__name__)
+
+@app.route("/")
+def onboarding():
+	return render_template("onboardform.html", title = 'DC2.0/Polaris - OnBoarding')
+
+if __name__ == "__main__":
+    app.run()
